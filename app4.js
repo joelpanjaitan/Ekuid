8589550a018fd56bc1bc4b806566eb6a05f7eaa1
joelpanjaitan <metrohal88@gmail.com>
2021-12-11 23:51:51 +0700

    number 4 answer and schema also diagram

--- app4.js
+++ app4.js
@@ -1,3 +1,6 @@
 //Database Schema dan Application Architecture Diagram untuk sebuah webapp perusahaan yang
 //bisa mencari dan menampilkan buku yang tersedia di perpustakaan
 //serta mencatat transaksi peminjaman dan pengembalian buku
+
+// 1. Untuk database schema di No_4_Schema
+// 2. Untuk application diagram di No_4_Diagram
